cd747ce9718828d8da6a0e05e62e3a45f6f96cff
BuildTools <unconfigured@null.spigotmc.org>
2024-04-02 12:44:36 -0400

    chatgpt is cooking🔥

--- src/me/stevemmmmm/thepitremake/utils/MapSorter.java
+++ src/me/stevemmmmm/thepitremake/utils/MapSorter.java
@@ -1,34 +1,24 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package me.stevemmmmm.thepitremake.utils;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class MapSorter {
     public MapSorter() {
     }
 
     public static HashMap<UUID, Integer> sortByValue(HashMap<UUID, Integer> hm) {
-        List<Map.Entry<UUID, Integer>> list = new LinkedList(hm.entrySet());
-        list.sort(Entry.comparingByValue());
-        HashMap<UUID, Integer> temp = new LinkedHashMap();
-        Iterator var3 = list.iterator();
-
-        while(var3.hasNext()) {
-            Map.Entry<UUID, Integer> aa = (Map.Entry)var3.next();
-            temp.put(aa.getKey(), aa.getValue());
-        }
-
-        return temp;
+        return hm.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue,
+                        LinkedHashMap::new
+                ));
     }
 }
+
