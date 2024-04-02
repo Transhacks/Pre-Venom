dc02f965612551d88b0cd826fbfa71dedac7f75e
BuildTools <unconfigured@null.spigotmc.org>
2024-04-02 10:55:33 -0400

    now you can read xD

--- src/me/stevemmmmm/thepitremake/game/ChatManagement.java
+++ src/me/stevemmmmm/thepitremake/game/ChatManagement.java
@@ -1,58 +1,88 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package me.stevemmmmm.thepitremake.game;
 
-import java.util.HashMap;
-import java.util.UUID;
 import me.stevemmmmm.permissions.core.PermissionsManager;
 import me.stevemmmmm.thepitremake.commands.MuteChatCommand;
 import me.stevemmmmm.thepitremake.core.Main;
 import me.stevemmmmm.thepitremake.managers.other.GrindingSystem;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-public class ChatManagement implements Listener {
-    private final HashMap<UUID, Integer> tasks = new HashMap();
-    private final HashMap<UUID, Integer> time = new HashMap();
+import java.util.HashMap;
+import java.util.UUID;
 
-    public ChatManagement() {
-    }
+public class ChatManagement implements Listener {
+    private final HashMap<UUID, Integer> tasks = new HashMap<>();
+    private final HashMap<UUID, Integer> time = new HashMap<>();
 
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
-        if (this.tasks.containsKey(event.getPlayer().getUniqueId()) && !event.getPlayer().isOp() && !event.getPlayer().hasPermission("bhperms.chatcooldownbypass")) {
-            event.setCancelled(true);
-            event.getPlayer().sendMessage(Main.prefix + " Your chat is on cooldown for " + ChatColor.YELLOW + this.time.get(event.getPlayer().getUniqueId()) + "s" + ChatColor.RED + "!");
-        } else if (MuteChatCommand.chatIsToggledOff && !event.getPlayer().hasPermission("bhperms.mutebypass")) {
+        UUID playerId = event.getPlayer().getUniqueId();
+
+        if (hasChatCooldown(playerId) && !canBypassChatCooldown(event.getPlayer())) {
+            cancelChatEvent(event, playerId);
+            return;
+        }
+
+        if (MuteChatCommand.chatIsToggledOff && !canBypassMute(event.getPlayer())) {
             event.getPlayer().sendMessage(Main.prefix + ChatColor.RED + "The chat is currently muted!");
             event.setCancelled(true);
-        } else {
-            String message = event.getMessage();
-            message = message.replace("%", "%%");
-            if (event.getPlayer().getCustomName() != null && !event.getPlayer().getCustomName().equals(event.getPlayer().getName())) {
-                event.setFormat(GrindingSystem.getInstance().getFormattedPlayerLevel(event.getPlayer()) + " " + PermissionsManager.getInstance().getPlayerRank(event.getPlayer()).getPrefix() + " " + PermissionsManager.getInstance().getPlayerRank(event.getPlayer()).getNameColor() + event.getPlayer().getCustomName() + " (" + event.getPlayer().getName() + ")" + ChatColor.WHITE + ": " + PermissionsManager.getInstance().getPlayerRank(event.getPlayer()).getChatColor() + message);
-            } else {
-                event.setFormat(GrindingSystem.getInstance().getFormattedPlayerLevel(event.getPlayer()) + " " + PermissionsManager.getInstance().getPlayerRank(event.getPlayer()).getPrefix() + " " + PermissionsManager.getInstance().getPlayerRank(event.getPlayer()).getNameColor() + event.getPlayer().getName() + ChatColor.WHITE + ": " + PermissionsManager.getInstance().getPlayerRank(event.getPlayer()).getChatColor() + message);
-            }
-
-            if (!this.tasks.containsKey(event.getPlayer().getUniqueId())) {
-                this.tasks.put(event.getPlayer().getUniqueId(), Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(Main.INSTANCE, () -> {
-                    this.time.put(event.getPlayer().getUniqueId(), (Integer)this.time.getOrDefault(event.getPlayer().getUniqueId(), 4) - 1);
-                    if ((Integer)this.time.get(event.getPlayer().getUniqueId()) <= 0) {
-                        Bukkit.getServer().getScheduler().cancelTask((Integer)this.tasks.get(event.getPlayer().getUniqueId()));
-                        this.tasks.remove(event.getPlayer().getUniqueId());
-                        this.time.remove(event.getPlayer().getUniqueId());
-                    }
-
-                }, 0L, 20L));
-            }
-
+            return;
         }
+
+        formatChatMessage(event);
+        startChatCooldown(event.getPlayer());
+    }
+
+    private boolean hasChatCooldown(UUID playerId) {
+        return tasks.containsKey(playerId);
+    }
+
+    private boolean canBypassChatCooldown(Player player) {
+        return player.isOp() || player.hasPermission("bhperms.chatcooldownbypass");
+    }
+
+    private boolean canBypassMute(Player player) {
+        return player.hasPermission("bhperms.mutebypass");
+    }
+
+    private void cancelChatEvent(AsyncPlayerChatEvent event, UUID playerId) {
+        event.setCancelled(true);
+        event.getPlayer().sendMessage(Main.prefix + " Your chat is on cooldown for " + ChatColor.YELLOW + time.get(playerId) + "s" + ChatColor.RED + "!");
+    }
+
+    private void formatChatMessage(AsyncPlayerChatEvent event) {
+        String message = event.getMessage().replace("%", "%%");
+        String playerName = event.getPlayer().getName();
+        String customName = event.getPlayer().getCustomName();
+        String formattedName = GrindingSystem.getInstance().getFormattedPlayerLevel(event.getPlayer()) +
+                " " + PermissionsManager.getInstance().getPlayerRank(event.getPlayer()).getPrefix() +
+                " " + PermissionsManager.getInstance().getPlayerRank(event.getPlayer()).getNameColor() +
+                (customName != null && !customName.equals(playerName) ? customName + " (" + playerName + ")" : playerName) +
+                ChatColor.WHITE + ": " +
+                PermissionsManager.getInstance().getPlayerRank(event.getPlayer()).getChatColor() +
+                message;
+        event.setFormat(formattedName);
+    }
+
+    private void startChatCooldown(Player player) {
+        if (!tasks.containsKey(player.getUniqueId())) {
+            tasks.put(player.getUniqueId(), Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(Main.INSTANCE, () -> {
+                int remainingTime = time.getOrDefault(player.getUniqueId(), 4) - 1;
+                time.put(player.getUniqueId(), remainingTime);
+                if (remainingTime <= 0) {
+                    cancelChatCooldownTask(player);
+                }
+            }, 0L, 20L));
+        }
+    }
+
+    private void cancelChatCooldownTask(Player player) {
+        Bukkit.getServer().getScheduler().cancelTask(tasks.get(player.getUniqueId()));
+        tasks.remove(player.getUniqueId());
+        time.remove(player.getUniqueId());
     }
 }
